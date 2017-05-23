dc699880754439fc86f0ee1a8e96b69b0b1d6b0c
safwaf <safwafsafwaf@gmail.com>
2017-05-23 12:34:09 -0600

    Room Infrastructure
    Started fleshing out PacmanGhost and building the infrastructure for door locking

--- GameObject.cs
+++ GameObject.cs
@@ -35,9 +35,33 @@ namespace GDIgame
         }
     }
 
+    class Control : GameObject
+    {
+        //this is the base for control objects.
+        //every room type has it's own control object that decides when to open the locked doors
+        public override void Step()
+        {
+            //do not override this, or the doors will never open
+            if (CheckCriteria())
+            {
+                foreach(LockedDoor d in myRoom.Objects)
+                {
+
+                }
+            }
+        }
+
+        public virtual bool CheckCriteria()
+        {
+            //ALWAYS override this, or the doors will always remain open
+            return true;
+        }
+    }
+
     class LockedDoor : GameObject
     {
         public static new Image sprite = Image.FromFile("LockedDoor.bmp");
+        bool active = true;
     }
 
     class Enemy: GameObject
@@ -51,12 +75,68 @@ namespace GDIgame
     }
     class PacmanGhost : Enemy
     {
-        public static new Image sprite = Image.FromFile("Ghost.bmp");
+        //later, we will diversify this into different colours of ghost who use different movement strategies
 
+        public static new Image sprite = Image.FromFile("Ghost.bmp");
+        public direction dir;   //try to set the direction when creating, so it makes sense
 
         public override void Step()
         {
-            
+            if (CheckMove())
+            {
+                //needs improvement. ghost should consider every possible turn, not just the turns it has to make
+                MoveForward();
+            }
+            else
+            {
+                NewDirection();
+                MoveForward();
+            }
+        }
+
+        public void MoveForward()
+        {
+            switch (dir)
+            {
+                case direction.up:
+                    y -= 1;
+                    break;
+                case direction.down:
+                    y += 1;
+                    break;
+                case direction.left:
+                    x -= 1;
+                    break;
+                case direction.right:
+                    x += 1;
+                    break;
+            }
+        }
+
+        public bool CheckMove()
+        {
+            switch (dir)
+            {
+                case direction.up:
+                    if (myRoom.walls[x, y - 1] == false) return true;
+                    break;
+                case direction.down:
+                    if (myRoom.walls[x, y + 1] == false) return true;
+                    break;
+                case direction.left:
+                    if (myRoom.walls[x-1, y] == false) return true;
+                    break;
+                case direction.right:
+                    if (myRoom.walls[x+1,y] == false) return true;
+                    break;
+            }
+            //otherwise, return false
+            return false;
+        }
+
+        public void NewDirection()
+        {
+
         }
     }
 }
